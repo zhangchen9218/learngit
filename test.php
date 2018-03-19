a6f73905c6f2c6b02dc2e41e6c534a243b202816
test <zhangchen@qq.com>
2018-03-19 16:50:51 +0800

    更新笔记

--- test.php
+++ test.php
@@ -10,4 +10,7 @@ git reset --hard HEAD^    //回退到上个一版本
 git reset --hard <提交的编号>  //退回到指定版本
 git reglog    //获取执行过的提交编号
 git checkout --<file>  //把指定文件在工作区的修改全部撤销
-git rm       //删除一个文件 
+git rm       //删除一个文件
+git remote add [origin] git@<server-name>:<path>/<repo-name>.git  //关联一个远程库
+git push [-u] [origin] [master]    //推送分支上的所有内容
+git pull [origin] [master]         //拉取远程
