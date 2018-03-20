c75019a412786d6b5c58a52b4a85746ce56059aa
test <zhangchen@qq.com>
2018-03-20 11:14:10 +0800

    提交笔记分支命令提交到指定分支

--- test.php
+++ test.php
@@ -14,3 +14,7 @@ git rm       //删除一个文件
 git remote add [origin] git@<server-name>:<path>/<repo-name>.git  //关联一个远程库
 git push [-u] [origin] [master]    //推送分支上的所有内容
 git pull [origin] [master]         //拉取远程
+git checkout [-b] <分支名称>       //创建并且切换分支
+git checkout <分支名称>            //切换分支
+git branch <分支名称>              //查看当前分支[列出所有分支，当前分支钱会标有*号]
+
